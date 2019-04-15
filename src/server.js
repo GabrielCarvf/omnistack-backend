371576de5b78c3ea4c86c5bd94bf2d4f13cba982
Gabriel <gabrielcarvalho33527@gmail.com>
2019-04-15 07:57:14 -0300

    att

--- src/server.js
+++ src/server.js
@@ -25,12 +25,13 @@ mongoose.connect(
 
 app.use((req, res, next) => {
     req.io = io;
+
     return next();
 });
 
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 
 app.use(require('./routes'));
