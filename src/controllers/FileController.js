136afbb5fb3c1c7d28439474f2fa8d317b544763
Gabriel <gabrielcarvalho33527@gmail.com>
2019-04-15 08:41:59 -0300

    att

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -2,7 +2,7 @@ const Box = require('../models/Box');
 const File = require('../models/File');
 
 class FileController {
-    async store (req, res){
+    async store (req, res) {
         const box = await Box.findById(req.params.id);
 
         const file = await File.create({
@@ -11,6 +11,7 @@ class FileController {
         });
 
         box.files.push(file);
+        
         await box.save();
         
         req.io.socckets.in(box._id).emit('file', file);
